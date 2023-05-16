7508c561746add526a9f12911115df317a4afb0a
Luis Fernando <luis.silva9902@gmail.com>
2023-05-16 09:54:57 -0300

    Working with performance using the React Devtools extension, the 'use-context-selector' library, useCallback and memo

--- 03-dt-money/src/components/NewTransactionModal/index.tsx
+++ 03-dt-money/src/components/NewTransactionModal/index.tsx
@@ -11,7 +11,7 @@ import * as z from 'zod'
 import { Controller, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { TransactionsContext } from '../../contexts/TransactionsContext'
-import { useContext } from 'react'
+import { useContextSelector } from 'use-context-selector'
 
 const newTransactionModalSchema = z.object({
   description: z.string(),
@@ -23,7 +23,13 @@ const newTransactionModalSchema = z.object({
 type NewTransactionModalSchemaType = z.infer<typeof newTransactionModalSchema>
 
 export function NewTransactionModal() {
-  const { createTransaction } = useContext(TransactionsContext)
+  const createTransaction = useContextSelector(
+    TransactionsContext,
+    (context) => {
+      return context.createTransaction
+    },
+  )
+
   const {
     control,
     register,
--- 03-dt-money/src/contexts/TransactionsContext.tsx
+++ 03-dt-money/src/contexts/TransactionsContext.tsx
@@ -1,5 +1,6 @@
-import { ReactNode, createContext, useEffect, useState } from 'react'
+import { ReactNode, useCallback, useEffect, useState } from 'react'
 import { api } from '../lib/axios'
+import { createContext } from 'use-context-selector'
 
 interface Transaction {
   id: number
@@ -27,7 +28,7 @@ export const TransactionsContext = createContext({} as TransactionContextType)
 export function TransactionsProvider({ children }: TransactionProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([])
 
-  async function fetchTransactions(query?: string) {
+  const fetchTransactions = useCallback(async (query?: string) => {
     const response = await api.get('/transactions', {
       params: {
         _sort: 'createdAt',
@@ -37,27 +38,28 @@ export function TransactionsProvider({ children }: TransactionProviderProps) {
     })
 
     setTransactions(response.data)
-  }
+  }, [])
 
-  async function createTransaction(
-    data: Omit<Transaction, 'id' | 'createdAt'>,
-  ) {
-    const { description, category, price, type } = data
+  const createTransaction = useCallback(
+    async (data: Omit<Transaction, 'id' | 'createdAt'>) => {
+      const { description, category, price, type } = data
 
-    const response = await api.post('/transactions', {
-      description,
-      category,
-      price,
-      type,
-      createdAt: new Date(),
-    })
+      const response = await api.post('/transactions', {
+        description,
+        category,
+        price,
+        type,
+        createdAt: new Date(),
+      })
 
-    setTransactions((state) => [...state, response.data])
-  }
+      setTransactions((state) => [...state, response.data])
+    },
+    [],
+  )
 
   useEffect(() => {
     fetchTransactions()
-  }, [])
+  }, [fetchTransactions])
 
   return (
     <TransactionsContext.Provider
