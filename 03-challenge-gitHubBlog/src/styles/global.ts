d7fc960bb91a2f99663555af2e29e3d28a2581a2
Luis Fernando <luis.silva9902@gmail.com>
2023-05-21 10:53:09 -0300

    Project created with React with Vite, added the styled-components, react-router-dom and fontawesome libraries, and made the initial project settings

---
+++ 03-challenge-gitHubBlog/src/styles/global.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  :focus {
+    outline: 0;
+  }
+
+  body {
+    background-color: ${(props) => props.theme['base-background']};
+    color: ${(props) => props.theme['base-text']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button, textarea {
+    font-family: 'Nunito', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+`
