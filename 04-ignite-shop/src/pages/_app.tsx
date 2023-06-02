e8d8e31923b056febeb5a8c94bd96d1d7fb74f0f
Luis Fernando <luis.silva9902@gmail.com>
2023-06-02 14:03:12 -0300

    Installed and configured the Stitches library, created the Header component, added the images with the 'Image' element of NextJS to optimize the images, created the structure of the homepage and created the carousel with the keen-slider library

--- 04-ignite-shop/src/pages/_app.tsx
+++ 04-ignite-shop/src/pages/_app.tsx
@@ -1,15 +1,26 @@
+import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
 import { Roboto } from 'next/font/google'
+import logoImg from '../assets/logo.svg'
+import Image from 'next/image'
+import { Container, Header } from '@/styles/pages/app'
 
 const roboto = Roboto({
   weight: ["400", "700"],
   subsets: ['latin']
 })
 
+globalStyles()
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={roboto.className}>
+    <Container className={roboto.className}>
+      <Header>
+        <Image src={logoImg} alt=''/>
+        <h1>Header</h1>
+      </Header>
       <Component {...pageProps} />
-    </main>
+    </Container>
+
   )
 }
