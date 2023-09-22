7af743ca5d03a2d54b1f5a52aace4bf51341b336
Luis Fernando <luis.silva9902@gmail.com>
2023-09-22 19:17:53 -0300

    feat: Add sidebar component

--- v2/01-react-fundamentals/src/App.jsx
+++ v2/01-react-fundamentals/src/App.jsx
@@ -1,10 +1,20 @@
 import { Header } from './components/Header'
 import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar'
 
 export function App() {
   return (
-    <>
-      <Header></Header>
-    </>
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          
+        </main>
+      </div>
+    </div>
   )
 }
