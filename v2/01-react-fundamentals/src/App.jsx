a0e0d383faf94c881bca1eaaee540de2051971d1
Luis Fernando <luis.silva9902@gmail.com>
2023-09-25 21:48:53 -0300

    refactor: change App component

--- v2/01-react-fundamentals/src/App.jsx
+++ v2/01-react-fundamentals/src/App.jsx
@@ -4,6 +4,55 @@ import styles from './App.module.css'
 import { Sidebar } from './components/Sidebar'
 import { Post } from './components/Post'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/luisfcodes.png',
+      name: 'Luis Fernando',
+      role: 'Software Engineer'
+    },
+    content: [
+      {
+        type: 'paragraph',
+        content: 'Fala galeraa 👋'
+      },
+      {
+        type: 'paragraph',
+        content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'
+      },
+      {
+        type: 'link',
+        content: 'jane.design/doctorcare',
+      }
+    ],
+    publishedAt: new Date('2023-07-03T12:00:00.000Z'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO Rocketseat'
+    },
+    content: [
+      {
+        type: 'paragraph',
+        content: 'Fala galeraa 👋'
+      },
+      {
+        type: 'paragraph',
+        content: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"
+      },
+      {
+        type: 'link',
+        content: 'jane.design/doctorcare',
+      }
+    ],
+    publishedAt: new Date('2023-07-03T12:00:00.000Z'),
+  }
+]
+
 export function App() {
   return (
     <div>
@@ -13,8 +62,14 @@ export function App() {
         <Sidebar />
 
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => (
+            <Post 
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          ))}
         </main>
       </div>
     </div>
