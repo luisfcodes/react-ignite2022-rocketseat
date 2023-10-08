6a0a0c4a28bf565ed8d31a6b87160c7be180801f
Luis Fernando <luis.silva9902@gmail.com>
2023-10-08 17:43:03 -0300

    feat: create routes with react-router-dom, create layout and create header component

--- v2/02-ignite-timer/src/App.tsx
+++ v2/02-ignite-timer/src/App.tsx
@@ -1,22 +1,15 @@
-import { useState } from 'react'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyles } from './styles/global'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>React TypeScript Starter</h1>
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <p>
-        <button onClick={() => setCount(count + 1)}>Click Me</button>
-        <span>Clicked {count} times</span>
-      </p>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyles />
     </ThemeProvider>
   )
