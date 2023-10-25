e6c9579d0cbb539295d53b5154ddae3acce014aa
Luis Fernando <luis.silva9902@gmail.com>
2023-10-24 21:03:55 -0300

    feat: create DefaultTheme and add fonts

--- v2/02-challenge-coffeeDelivery/src/App.tsx
+++ v2/02-challenge-coffeeDelivery/src/App.tsx
@@ -1,9 +1,15 @@
+import { ThemeProvider } from "styled-components"
 import { Container } from "./layout/Default"
+import { GlobalStyle } from "./styles/global"
+import defaultTheme from "./styles/themes/DefaultTheme"
 
 export function App() {
   return (
-    <Container>
-      <h1>Hello World</h1>
-    </Container>
+    <ThemeProvider theme={defaultTheme}>
+      <Container>
+        <h1>Hello World</h1>
+      </Container>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
