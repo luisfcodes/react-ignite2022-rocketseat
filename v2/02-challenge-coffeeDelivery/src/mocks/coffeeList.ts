ef87d1231ad362d3becdce143a32151517ea80fd
Luis Fernando <luis.silva9902@gmail.com>
2023-11-01 19:03:15 -0300

    feat: create mock to coffeeList and add coffee images

---
+++ v2/02-challenge-coffeeDelivery/src/mocks/coffeeList.ts
@@ -0,0 +1,86 @@
+export const coffeeList = [
+  {
+    imgUrl: 'traditional_express.png',
+    name: 'Expresso Tradicional',
+    attributes: ['tradicional'],
+    description: "O tradicional café feito com água quente e grãos moídos"
+  },
+  {
+    imgUrl: 'american_express.png',
+    name: 'Expresso Americano',
+    attributes: ['tradicional'],
+    description: "Expresso diluído, menos intenso que o tradicional"
+  },
+  {
+    imgUrl: 'creamy_express.png',
+    name: 'Expresso Cremoso',
+    attributes: ['tradicional'],
+    description: "Café expresso tradicional com espuma cremosa"
+  },
+  {
+    imgUrl: 'iced_express.png',
+    name: 'Expresso Gelado',
+    attributes: ['tradicional', 'gelado'],
+    description: "Bebida preparada com café expresso e cubos de gelo"
+  },
+  {
+    imgUrl: 'coffee_with_milk.png',
+    name: 'Café com Leite',
+    attributes: ['tradicional', 'com leite'],
+    description: "Meio a meio de expresso tradicional com leite vaporizado"
+  },
+  {
+    imgUrl: 'latte.png',
+    name: 'Latte',
+    attributes: ['tradicional', 'com leite'],
+    description: "Uma dose de café expresso com o dobro de leite e espuma cremosa"
+  },
+  {
+    imgUrl: 'capuccino.png',
+    name: 'Capuccino',
+    attributes: ['tradicional', 'com leite'],
+    description: "Bebida com canela feita de doses iguais de café, leite e espuma"
+  },
+  {
+    imgUrl: 'macchiato.png',
+    name: 'Macchiato',
+    attributes: ['tradicional', 'com leite'],
+    description: "Café expresso misturado com um pouco de leite quente e espuma"
+  },
+  {
+    imgUrl: 'mocaccino.png',
+    name: 'Mocaccino',
+    attributes: ['tradicional', 'com leite'],
+    description: "Café expresso com calda de chocolate, pouco leite e espuma"
+  },
+  {
+    imgUrl: 'hot_chocolate.png',
+    name: 'Chocolate Quente',
+    attributes: ['especial', 'com leite'],
+    description: "Bebida feita com chocolate dissolvido no leite quente e café"
+  },
+  {
+    imgUrl: 'cuban.png',
+    name: 'Cubano',
+    attributes: ['especial', 'alcoólico', 'gelado'],
+    description: "Drink gelado de café expresso com rum, creme de leite e hortelã"
+  },
+  {
+    imgUrl: 'hawaiian.png',
+    name: 'Havaiano',
+    attributes: ['especial'],
+    description: "Bebida adocicada preparada com café e leite de coco"
+  },
+  {
+    imgUrl: 'arabic.png',
+    name: 'Árabe',
+    attributes: ['especial'],
+    description: "Bebida preparada com grãos de café árabe e especiarias"
+  },
+  {
+    imgUrl: 'irish.png',
+    name: 'Irlandês',
+    attributes: ['especial', 'alcoólico'],
+    description: "Bebida a base de café, uísque irlandês, açúcar e chantilly"
+  },
+]
