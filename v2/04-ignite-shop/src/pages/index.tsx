70a1724fc8c4652ef450e0b73c50958d055cc30f
Luis Fernando <luis.silva9902@gmail.com>
2023-12-13 22:45:58 -0300

    feat: add getStaticProps

--- v2/04-ignite-shop/src/pages/index.tsx
+++ v2/04-ignite-shop/src/pages/index.tsx
@@ -5,7 +5,7 @@ import { useKeenSlider } from "keen-slider/react";
 
 import { stripe } from "@/lib/stripe";
 import "keen-slider/keen-slider.min.css";
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Stripe from "stripe";
 
 interface HomeProps {
@@ -41,7 +41,7 @@ export default function Home({ products }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ["data.default_price"],
   });
@@ -63,5 +63,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       products,
     },
+    revalidate: 60 * 60 * 2, // 2 hours
   };
 };
