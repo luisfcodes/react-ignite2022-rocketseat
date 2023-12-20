9aa2ee12eeaabf8bec42a12be7600fbe50fac805
Luis Fernando <luis.silva9902@gmail.com>
2023-12-19 21:39:29 -0300

    feat: send priceId to api route

--- v2/04-ignite-shop/src/pages/api/checkout.ts
+++ v2/04-ignite-shop/src/pages/api/checkout.ts
@@ -5,7 +5,19 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const priceId = "price_1NEbZjDM8skj1y4EVv83ZlAz";
+  const { priceId } = req.body;
+
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      error: "Method not allowed",
+    });
+  }
+
+  if (!priceId) {
+    return res.status(400).json({
+      error: "Price ID not provided",
+    });
+  }
 
   const successUrl = `${process.env.NEXT_URL}/success`;
   const cancelUrl = `${process.env.NEXT_URL}/`;
