2094068e18b1b757222fb83e35a56ed9ec582c97
Luis Fernando <luis.silva9902@gmail.com>
2023-12-23 16:27:35 -0300

    feat: create Heading component

---
+++ v2/05-design-system/packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import { Text, TextProps } from '@ignite-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum omnis commodi error quas, a ratione ipsum alias iste, vero blanditiis sequi est magnam repellendus labore ex qui ab esse autem!',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
