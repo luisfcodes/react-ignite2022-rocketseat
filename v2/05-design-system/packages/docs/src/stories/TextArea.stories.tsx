9071c241df8bb383b4e5bb5163378e24c403a2f4
Luis Fernando <luis.silva9902@gmail.com>
2023-12-23 19:52:33 -0300

    feat: create TextArea component

---
+++ v2/05-design-system/packages/docs/src/stories/TextArea.stories.tsx
@@ -0,0 +1,31 @@
+import { Box, Text, TextArea, TextAreaProps } from '@ignite-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Form/Text Area',
+  component: TextArea,
+  args: {},
+  decorators: [
+    (story) => (
+      <Box
+        as="label"
+        css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+      >
+        <Text size="sm">Observations</Text>
+        {story()}
+      </Box>
+    ),
+  ],
+} as Meta<TextAreaProps>
+
+export const Primary: StoryObj<TextAreaProps> = {
+  args: {
+    placeholder: 'Type any observations...',
+  },
+}
+
+export const Disabled: StoryObj<TextAreaProps> = {
+  args: {
+    disabled: true,
+  },
+}
